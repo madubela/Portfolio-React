e80f0d46bfc4d2a61ba0dec277e01b343b753b7e
LangaMadubela <madubelamanelisi3@gmail.com>
2024-02-23 15:22:06 +0200

    MADUBELA

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,11 +19,12 @@ const About = () => {
         <p>
         I'm currently undertaking an 18-month internship in Software Development at CapaCiTi, which provides me with valuable hands-on experience in the field.
         </p>
+        <h1>I am Madubela</h1>
         <p>
         Beyond my professional pursuits, I have a passion for playing the keyboard, which I find to be a therapeutic and enjoyable pastime. I come from a supportive family of five, which includes my mother, Mamtshawe, and my three brothers. I am the youngest member of our family.
         </p>
         <img src= {Image} alt="Madubela langa" className="about-img" />
-        <h1>I am Madubela</h1>
+        
       
     </div>
    </section>
